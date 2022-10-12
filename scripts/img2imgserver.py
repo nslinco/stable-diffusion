00fd466c1d6b53e17331f7e10f6fc62affcbab61
Noah <nslinco@gmail.com>
2022-10-11 18:14:20 -0600

    Updated servers

--- scripts/img2imgserver.py
+++ scripts/img2imgserver.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 from io import BytesIO
 import time
+import requests
+from threading import Thread
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
@@ -16,7 +18,11 @@ CORS(app)
 print("--> Starting Stable Diffusion img2img Server. This might take up to two minutes.")
 
 from img2imgmodel import SDModel
-sd_model = None
+img2img_model = None
+
+curJobs = {
+    'img2img': None
+}
 
 parser = argparse.ArgumentParser(description = "A Stable Diffusion img2img app to turn your textual prompts into visionary delights")
 parser.add_argument("--port", type=int, default=8000, help = "backend port")
@@ -25,38 +31,61 @@ parser.add_argument("--img_format", type = str.lower, default = "JPEG", help = "
 parser.add_argument("--output_dir", type = str, default = DEFAULT_IMG_OUTPUT_DIR, help = "Customer directory for generated images")
 args, unknown = parser.parse_known_args()
 
-@app.route("/img2img", methods=["POST"])
-@cross_origin()
-def generate_images_api():
-    json_data = request.get_json(force=True)
-    image = json_data["image"]
-    prompt = json_data["prompt"]
-    num_steps = json_data["num_steps"]
-    generated_imgs = sd_model.generate_image(BytesIO(base64.b64decode(image)), prompt, num_steps)
-
-    returned_generated_images = []
-    # if args.save_to_disk: 
-    #     dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H:%M:%S')}_{text_prompt}")
-    #     Path(dir_name).mkdir(parents=True, exist_ok=True)
-    
-    # for idx, img in enumerate(generated_img):
-        # if args.save_to_disk: 
-        #   img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
-
+def encodeImgs(generated_imgs):
+    returned_generated_images = []    
     for idx, img in enumerate(generated_imgs):
-        # if args.save_to_disk: 
-        #   img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
-
         buffered = BytesIO()
         img.save(buffered, format=args.img_format)
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        img_str = f'data:image/{args.img_format};base64,{img_str}'
         returned_generated_images.append(img_str)
+    return (returned_generated_images)
 
-    # print(f"Created {num_images} images from text prompt [{text_prompt}]")
+def postResponse(jobId, response):
+    url = 'https://feedback-backend.herokuapp.com/create/callback'
+    myobj = {}
+    myobj[jobId] = response
+    x = requests.post(url, data=myobj)
+    return (x)
+
+def doImg2Img():
+    # Parse request
+    job = curJobs['img2img']
+    jobId = job['_id']
+    jobData = job['data']
+    image = jobData["image"]
+    prompt = jobData["prompt"]
+    num_images = jobData["num_images"]
+    num_steps = jobData["num_steps"]
+
+    # Generate Images
+    generated_imgs = img2img_model.generate_images(BytesIO(base64.b64decode(image)), prompt, num_images, num_steps)
+    curJobs['img2img'] = None
+
+    # Encode Images
+    returned_generated_images = encodeImgs(generated_imgs)
     
-    response = {'generatedImgs': returned_generated_images,
-    'generatedImgsFormat': args.img_format}
-    return jsonify(response)
+    # Return Images
+    return postResponse(jobId, jsonify({
+        'generatedImgs': returned_generated_images,
+        'generatedImgsFormat': args.img_format
+    }))
+
+@app.route("/img2img", methods=["POST"])
+@cross_origin()
+def img2img_api():
+    # Return if busy
+    if (curJobs['img2img']): return jsonify(curJobs['img2img']['_id'], 'waiting')
+
+    # Parse request
+    job = request.get_json(force=True)
+
+    # Run Job
+    curJobs['img2img'] = job
+    Thread(target = doImg2Img()).start()
+    
+    # Report Job has started
+    return jsonify(job['_id'], 'working')
 
 
 @app.route("/", methods=["GET"])
@@ -66,8 +95,11 @@ def health_check():
 
 
 with app.app_context():
-    sd_model = SDModel()
-    #ip_model.generate_image("warmup.png", "warmup_mask.png", 50)
+    # Initialize Models
+    img2img_model = SDModel()
+
+    # Run Warm-Up Tests
+    # t1 = img2img_model.generate_images(BytesIO(base64.b64decode(tImg)), prompt, 1, 50)
     print("--> Stable Diffusion img2img Server is up and running!")
 
 
